74530e1f819781ac49d71db8bc5b6dca67250f92
nicole-deng4 <nd6753@princeton.edu>
2025-02-08 00:21:17 -0500

    Printing forward slash

--- decomment.c
+++ decomment.c
@@ -56,6 +56,7 @@ static int handleStartCommentState (int c)
     }
     else if (c == '/')
     {
+        putchar ('/');
         CURRENT_STATE = START_COMMENT;
     }
     else /* Uncomplete start of a comment*/
